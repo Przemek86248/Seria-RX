dc375dac7c084e890e9d99651465e9a909a63c1a
Przemek Sroka <przemek.sroka150@wp.pl>
2024-10-04 23:47:22 +0200

    Add new feture Sliders but it is't complete

--- skrypt.js
+++ skrypt.js
@@ -6,3 +6,31 @@ collapsible.forEach((item) => {
         targetElement.classList.toggle("collapsible--expanded");
     });
 });
+
+const sliderimage = document.querySelector(".slider");
+let numberOfPhoto = document.querySelectorAll(".slider_img").length;
+let buttomNext = document.querySelector(".sliderNavNext");
+let buttomPrev = document.querySelector(".sliderNavPrev");
+let currentIndex = 0;
+
+function checkIndex() {
+    if (currentIndex < 0) {
+        currentIndex = numberOfPhoto - 1;
+    }
+    if (currentIndex >= numberOfPhoto) {
+        currentIndex = 0;
+    }
+}
+buttomNext.addEventListener("click", function() {
+    currentIndex++;
+    sliderimage.style.transform = `translateX(-${currentIndex * 56}rem)`;
+    checkIndex();
+});    
+
+
+buttomPrev.addEventListener("click", function() {
+    currentIndex--;
+    sliderimage.style.transform = `translateX(-${currentIndex * 56}rem)`;
+    checkIndex();
+});
+
